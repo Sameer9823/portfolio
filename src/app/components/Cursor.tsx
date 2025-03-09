3e313c3a1406024d19c02486bbf1a6fc113e209b
Sameer Selokar <sameerselokar924@gmail.com>
2025-03-09 13:17:46 +0530

    add pointer prototype function to Cursor component

--- src/app/components/Cursor.tsx
+++ src/app/components/Cursor.tsx
@@ -1,5 +1,23 @@
 import { SplashCursor } from "@/components/ui/splash-cursor"
 
+interface PointerPrototype {
+  id: number;
+  texcoordX: number;
+  texcoordY: number;
+  prevTexcoordX: number;
+}
+
+function pointerPrototype(): PointerPrototype {
+  return {
+    id: -1,
+    texcoordX: 0,
+    texcoordY: 0,
+    prevTexcoordX: 0,
+  };
+}
+
+export default pointerPrototype;
+
 export function NoiseDemo() {
   return (
     <SplashCursor />
